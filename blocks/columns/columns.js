039a062a09d3a0b6f24e519a860a0e72c593c061
iustinp <iustinp@adobe.com>
2025-06-17 12:47:11 +0300

    Styled collumns

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,37 @@
+function addInViewOnScroll() {
+  const elements = document.querySelectorAll('.columns > div > div:not([class])');
+
+  function checkInView() {
+    elements.forEach((el) => {
+      const rect = el.getBoundingClientRect();
+      const completelyInView = rect.top >= 0
+        && rect.left >= 0
+        && rect.bottom <= (window.innerHeight + 130 || document.documentElement.clientHeight + 130)
+        && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+      const lowerPartTouches = rect.bottom > window.innerHeight;
+      if (completelyInView) {
+        el.classList.add('in-view');
+        el.classList.remove('out-view');
+      } else if (lowerPartTouches) {
+        el.classList.remove('in-view');
+        el.classList.add('out-view');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkInView, { passive: true });
+  window.addEventListener('resize', checkInView);
+  checkInView();
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+    requestAnimationFrame(addInViewOnScroll);
+  });
+} else {
+  requestAnimationFrame(addInViewOnScroll);
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
