5e8877faee9e3b56ae02298c3fe845fb7a33009b
lgilbert <lgilbert@adobe.com>
2025-06-20 15:14:11 -0400

    Updated cards.css to style card blocks similar to Spectrum card block style. Added new feature to cards.js to allow a third column containing a link when using the card block that links the contents of the card. Created a draft for the index page containing new block content under /drafts/lgilbert/index.docx 	modified:   blocks/cards/cards.css 	modified:   blocks/cards/cards.js
    Fix #7
    
    Test URLs:
        Before: https://main--ams-lts--aemsites.aem.page/
        After: https://7-cards-style-main--ams-lts--aemsites.aem.page/drafts/lgilbert/
    
    Instructions for how to review:
    Scroll down to the card block section "Your AEM 6.5 LTS Upgrade Journey Starts Here", compare block styling to https://s2.spectrum.adobe.com/

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,12 +6,36 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+
+    // Assign classes to div elements before wrapping in a link
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
+
+    // Check if the third column exists and contains a link
+    const thirdColumn = li.children[2]; // Get the third child (0-based index)
+    if (thirdColumn && thirdColumn.querySelector('a')) {
+      const link = thirdColumn.querySelector('a'); // Get the link
+      const href = link.href; // Extract the href attribute
+
+      // Remove the text content of the link
+      link.textContent = ''; // Clears any text inside the <a> tag
+
+      // Wrap the entire <li> content in the link
+      const wrapperLink = document.createElement('a');
+      wrapperLink.href = href;
+      wrapperLink.style.display = 'block'; // Ensure the link covers the entire <li>
+      while (li.firstChild) wrapperLink.append(li.firstChild); // Move all children into the link
+      li.append(wrapperLink); // Append the link back to the <li>
+    }
+
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
